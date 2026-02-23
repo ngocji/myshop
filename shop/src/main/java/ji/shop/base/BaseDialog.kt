ad1fa17639c70705f7910e760ca46e14c659fcb5
ngocji <dngocx@gmail.com>
2026-02-23 11:19:45 +0700

    Update base dialog

--- shop/src/main/java/ji/shop/base/BaseDialog.kt
+++ shop/src/main/java/ji/shop/base/BaseDialog.kt
@@ -4,16 +4,22 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Window
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 
 abstract class BaseDialog(layoutRes: Int) : DialogFragment(layoutRes) {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return super.onCreateDialog(savedInstanceState).apply {
-            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            window?.setWindowAnimations(0)
+            window?.also { doOnWindow(it) }
         }
     }
+
+    open fun doOnWindow(window: Window) {
+        window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        window.setWindowAnimations(0)
+    }
+
     fun show(fragmentManager: FragmentManager) {
         show(fragmentManager, this::class.java.name)
     }
