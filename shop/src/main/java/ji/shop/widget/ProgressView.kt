74e80828776af5ad3e03af69ddbd899780f4cbeb
Ton Duong <ton.van.duong@gm.com>
2026-02-24 11:16:01 +0700

    Impl inventory

--- shop/src/main/java/ji/shop/widget/ProgressView.kt
+++ shop/src/main/java/ji/shop/widget/ProgressView.kt
@@ -1,19 +1,58 @@
 package ji.shop.widget
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.LinearLayout
-import android.widget.TextView
-import com.google.android.material.progressindicator.LinearProgressIndicator
+import ji.shop.R
+import ji.shop.databinding.ProgressViewBinding
 
 class ProgressView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, def: Int = 0
 ) : LinearLayout(context, attrs, def) {
 
-    private val progressBar: LinearProgressIndicator? = null
-    private val tvPercent: TextView? = null
+    private val binding = ProgressViewBinding.inflate(LayoutInflater.from(context), this)
 
     init {
+        attrs?.let {
+            val typedArray =
+                context.obtainStyledAttributes(it, R.styleable.ProgressItemView, def, 0)
 
+            val progress = typedArray.getInt(
+                R.styleable.ProgressItemView_progressValue, 0
+            )
+
+            val indicatorColor = typedArray.getColor(
+                R.styleable.ProgressItemView_indicatorColor, Color.BLUE
+            )
+
+            val trackColor = typedArray.getColor(
+                R.styleable.ProgressItemView_trackColor, Color.GRAY
+            )
+            binding.progressBar.apply {
+                setProgress(progress)
+                setIndicatorColor(indicatorColor)
+                setTrackColor(trackColor)
+            }
+        }
+        initViews()
+    }
+
+    private fun initViews() {
+
+    }
+
+    fun setProgress(percent: Int) {
+        binding.progressBar.setProgressCompat(percent, true)
+        binding.tvPercent.text = "$percent %"
+    }
+
+    fun setIndicatorColor(color: Int) {
+        binding.progressBar.setIndicatorColor(color)
+    }
+
+    fun setTrackColor(color: Int) {
+        binding.progressBar.trackColor = color
     }
 }
