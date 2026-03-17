2842dfcc078764969513257c6fb066433045b0bf
ngocji <dngocx@gmail.com>
2026-03-17 23:16:23 +0700

    Add refresh token

--- shop/src/main/java/ji/shop/ShopSDK.kt
+++ shop/src/main/java/ji/shop/ShopSDK.kt
@@ -1,10 +1,8 @@
 package ji.shop
 
 object ShopSDK {
-    private var authenticationToken: String =
-        "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE3Mjg0NTUyMDIsImlhdCI6MTcyODQ1NTAyMn0.jADTGEdN5YN-hmNoDvGJAnycU6IY-OKg8V98s1PaLSk"
-    private var accessToken: String =
-        "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE3NzM3NjA3MDQsImlhdCI6MTc3Mzc1OTgwNH0.bJEtjYfA46UEsV8sdsBACmGPuV4Jg-YaI7HoMJeERq8"
+    private var authenticationToken: String = ""
+    private var accessToken: String = ""
     private var refreshToken: String = "F6LAontbgGwbCbgAZiM5BA"
     private var venueId: String = "90"
 
